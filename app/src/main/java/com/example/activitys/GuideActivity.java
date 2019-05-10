9838de873ed5b41f01c0c0b1af9b6889a5cd6e79
EF <EF@ef.com>
2019-05-10 09:40:17 +0800

    引导页的红点移动

--- app/src/main/java/com/example/activitys/GuideActivity.java
+++ app/src/main/java/com/example/activitys/GuideActivity.java
@@ -1,19 +1,20 @@
 package com.example.activitys;
 
 import android.app.Activity;
-import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-
+import android.widget.RelativeLayout;
 import com.example.beijingnews.R;
-
 import java.util.ArrayList;
 
 /**
@@ -25,6 +26,11 @@ public class GuideActivity extends Activity {
     private Button btn_start_main;
     private LinearLayout ll_point_group;
 
+    private ImageView iv_red_point;
+
+    /// 两红点的间距
+    private int leftmax;
+
     private ArrayList<ImageView> imageViews;
 
     @Override
@@ -35,6 +41,8 @@ public class GuideActivity extends Activity {
         viewpager = findViewById(R.id.viewpager);
         btn_start_main = findViewById(R.id.btn_start_main);
         ll_point_group = findViewById(R.id.ll_point_group);
+        iv_red_point = findViewById(R.id.iv_red_point);  //红点的移动就是距离左边的margin动态改变
+
 
         //准备数据
         int[] ids = new int[]{R.drawable.guide_1, R.drawable.guide_2, R.drawable.guide_3,};
@@ -47,19 +55,108 @@ public class GuideActivity extends Activity {
             imageView.setBackgroundResource(ids[i]);
             //添加到集合中
             imageViews.add(imageView);
-        }
 
+            //创建点, 添加到线性布局里面
+            ImageView point = new ImageView(this);
+            point.setBackgroundResource(R.drawable.point_normal);
+
+            // 这里的宽高 单位是像素
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(50, 50);
+            if (i != 0) { //不包括第0个, 所有的点距离左边50个像素
+                params.leftMargin = 50;
+            }
+            point.setLayoutParams(params);
+
+            ll_point_group.addView(point);  //添加到线性布局里面
+
+        }
 
         //设置viewpager的适配器
         viewpager.setAdapter(new MyPagerAdapter());
 
+        //根据view的生命周期, 当视图执行到 onLayout 或者 onDraw 的时候,视图的宽和高,边距就都偶有了
+        iv_red_point.getViewTreeObserver().addOnGlobalLayoutListener(new MyGlobalLayoutLinstner());
+
+        //获得屏幕屏幕滑动的百分比
+        viewpager.addOnPageChangeListener(new MyPageChangeListener());
 
     }
 
 
-    /**
-     * The type My pager adapter.
-     */
+    // 页面滑动的时候,
+    class MyPageChangeListener implements ViewPager.OnPageChangeListener {
+
+        /**
+         * 当前页面会毁掉这个方法
+         *
+         * @param i  当前滑动页面的位置
+         * @param v  当前页面滑动的百分比
+         * @param i1 滑动的像素
+         */
+        @Override
+        public void onPageScrolled(int i, float v, int i1) {
+            //两点间移动的距离 = 屏幕滑动百分比 * 间距
+
+            // 直接这么用就没有计算点滑动的正确位置, 因为当第一页滑动的时候, 百分比这个参数就会 再次从0开始.
+            //int leftMargin = (int) (v * leftmax);  //请注意,强制类型转换的运算优先级是大于乘法优先级的,  (int) v * leftmax 这个表达式结果永远是 0
+
+            Log.i("A", "fjdkasljfas");
+
+            //两点间滑动距离对应的坐标 = 原来的其实位置 + 两点间移动的距离
+            int leftMargin = (int) (i * leftmax + v * leftmax);
+            // parama.leftMargin = 两点间滑动距离对应的坐标
+
+            RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams) iv_red_point.getLayoutParams();
+
+            params.leftMargin = leftMargin;
+
+            iv_red_point.setLayoutParams(params);
+
+        }
+
+        /**
+         * 当页面被选中的时候回调的方法
+         *
+         * @param i 被选中页面对应的位置
+         */
+        @Override
+        public void onPageSelected(int i) {
+
+        }
+
+        /**
+         * 当viewpager页面滑动状态发生改变的时候 好像是有三种状态
+         *
+         * @param i
+         */
+        @Override
+        public void onPageScrollStateChanged(int i) {
+
+        }
+    }
+
+
+    //页面布局的时候
+    class MyGlobalLayoutLinstner implements ViewTreeObserver.OnGlobalLayoutListener {
+
+        @Override
+        public void onGlobalLayout() {
+
+            Log.i("执行次数", "onGlobalLayout");
+
+            // 这个方法执行不止一次,需要移除, 这个移除的函数貌似对低版本不支持,根据编辑器提示修改成if
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+                iv_red_point.getViewTreeObserver().removeOnGlobalLayoutListener(this); //这里的 this 值得是 MyGlobalLayoutLinstner.this
+            }
+
+            //间距 = 第一个红点距离左边的距离 - 第0个红点距离左边的距离
+            leftmax = ll_point_group.getChildAt(1).getLeft() - ll_point_group.getChildAt(0).getLeft();
+
+        }
+    }
+
+
+    //分页视图的需要的代理方法 返回数据源
     class MyPagerAdapter extends PagerAdapter {  //这个类需要重写的方法有四个
 
         /// 返回数据的总个数
