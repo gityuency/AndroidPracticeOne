4262d6f7cf5ead654a1e226983306d00f61df409
EF <EF@ef.com>
2019-05-10 18:27:24 +0800

    16  替换左侧菜单和正文内容

--- app/src/main/java/com/example/activitys/MainActivity.java
+++ app/src/main/java/com/example/activitys/MainActivity.java
@@ -1,13 +1,21 @@
 package com.example.activitys;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 
 import com.example.beijingnews.R;
+import com.example.fragment.ContentFragment;
+import com.example.fragment.LeftmenuFragment;
 import com.example.utils.DensityUtil;
 import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
 import com.jeremyfeinstein.slidingmenu.lib.app.SlidingFragmentActivity;
 
 public class MainActivity extends SlidingFragmentActivity {
+
+    public static final String MAIN_CONTENT_TAG = "main_content_tag";
+    public static final String LEFTMENU_TAG = "leftmenu_tag";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -29,6 +37,26 @@ public class MainActivity extends SlidingFragmentActivity {
 
         //6.设置主页占据的宽度
         slidingMenu.setBehindOffset(DensityUtil.dip2px(MainActivity.this, 200));
+
+        // 初始化Fragment
+        initFragment();
+    }
+
+
+    private void initFragment() {
+
+        //1.得到FragmentManager 使用V4的  getSupportFragmentManager();   不要用这个: getFragmentManager()
+        FragmentManager fm = getSupportFragmentManager();
+
+        //2.开启事务
+        FragmentTransaction ft = fm.beginTransaction();
+
+        //3.替换  这个new出来的 ContentFragment 在当前Activity里面, 所以这里面我们定义的属性 public Activity context; 是可以取到的
+        ft.replace(R.id.fl_main_content, new ContentFragment(), MAIN_CONTENT_TAG);  //主页
+        ft.replace(R.id.fl_leftmenu, new LeftmenuFragment(), LEFTMENU_TAG);  //左侧菜单
+
+        //4.提交
+        ft.commit();
 
     }
 
