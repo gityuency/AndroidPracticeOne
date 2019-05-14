df9fbeee2a32166c3b02e0650fbdf32986ea2953
EF <EF@ef.com>
2019-05-14 10:29:53 +0800

    30 配置网络请求数据 只是写了一个基类

---
+++ app/src/main/java/com/example/utils/Constants.java
@@ -0,0 +1,22 @@
+package com.example.utils;
+
+/**
+ * 常量类
+ */
+public class Constants {
+
+    /**
+     * 请求的域名
+     */
+    public static final String BASE_URL = "";
+
+
+    // http://10.0.2.0:8080, 本地的模拟器,访问本机的tomcat服务器
+
+
+    /**
+     * 新闻中心的网络请求地址 字符串转化大写  command + shift + u    ctrl + shift + u 小写
+     */
+    public static final String NEWSCENTER_PAGER_URL = BASE_URL + "";
+
+}
